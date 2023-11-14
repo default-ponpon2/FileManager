98c678cbf20c8147ce95524df2ab938d8e22e3c4
capypon <amidfox85@gmail.com>
2023-11-14 17:31:14 +0500

    fixed exe file building

--- createexe.py
+++ createexe.py
@@ -1,8 +1,9 @@
 import os
 
 try:
+    print('Removing old FileManager.exe')
     os.remove('dist/FileManager.exe')
 except FileNotFoundError as e:
     pass
-
+print('Old File was deleted nahuy')
 os.system('pyinstaller --onefile FileManager.py')
