a5ee2b518aa8e2a10f2381399272e5d73f17a23a
capypon <amidfox85@gmail.com>
2023-11-15 16:22:28 +0500

    exe compiling with nuitka

---
+++ FileManager.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'FileManager.exe' with Python DLL being found.
+set PATH=d:\PROGRA~1\python;%PATH%
+set PYTHONHOME=D:\program files\python
+"%~dp0FileManager.exe" %*
