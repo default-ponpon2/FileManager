611060a5b735b554ffdc42dcfd072067808e2dfc
capypon <amidfox85@gmail.com>
2023-11-16 16:11:14 +0500

    removed all useless files

--- FileManager.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'FileManager.exe' with Python DLL being found.
-set PATH=d:\PROGRA~1\python;%PATH%
-set PYTHONHOME=D:\program files\python
-"%~dp0FileManager.exe" %*
